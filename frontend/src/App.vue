68ce7d5a12c9e2e40352c1a50111775e4fbcea0c
Kabilan-Hash <kabilan.bose@hashagile.com>
2024-11-04 15:30:21 +0530

    Add files via upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,34 @@
+
+
+<script>
+import  axios from 'axios'
+export default{
+  methods: {
+      uploadFile() {
+        this.Images = this.$refs.file.files[0];
+      },
+      submitFile() {
+        const formData = new FormData();
+        formData.append('file');
+        const headers = { 'Content-Type': 'multipart/form-data' };
+        axios.post('http://127.0.0.1:3000/resume_data/new', formData, { headers }).then((res) => {
+          res.data.files;
+          res.status; 
+        });
+      }
+    }
+
+}
+</script>
+
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status">Yeah it's working</p>
+  <form action="" method="post">
+    <input type="file" name="resume" id="resume">
+    <button type="submit" >Upload</button>
+  </form>
+  
+</template>
+
